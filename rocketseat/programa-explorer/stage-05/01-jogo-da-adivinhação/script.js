404a16b54cc829adc7029d78b6089f88c72f99fc
Claudiney <csld72k@gmail.com>
2024-01-31 11:45:43 -0300

    Altera algumas coisas no arquibo script.js

--- rocketseat/programa-explorer/stage-05/01-jogo-da-adivinhação/script.js
+++ rocketseat/programa-explorer/stage-05/01-jogo-da-adivinhação/script.js
@@ -1,8 +1,8 @@
 let gameScreen = document.querySelector(".gameScreen")
 let winScreen = document.querySelector(".winScreen")
-let inputNumber = document.querySelector(".inputNumber")
+// let inputNumber = document.querySelector("#inputNumber")
 let buttonTry = document.querySelector("#buttonTry")
-let randomNumber
+let randomNumber, attempts
 
 function generateRandomNumber() {
   return randomNumber = Math.round(Math.random() * 10)
@@ -10,13 +10,24 @@ function generateRandomNumber() {
 
 randomNumber = generateRandomNumber()
 
-function verifyIfTheAnswerIsValid() {
-  let chosenNumber = inputNumber
-  return
+console.log(randomNumber)
+
+function incorrectAttempts() {
+  /* 
+
+    Aqui vou fazer uma forma de ativar uma animação CSS que indique ao usuário que ele introduziu um resposta incorreta, algo como tremer o botão e colocar ele em vermelho ou algo do tipo.
+    !  Ainda vou fazer a animação.
+    
+  */
 }
 
 function verifyIfAnswerMatch() {
-  verifyIfTheAnswerIsValid()
+  let chosenNumber = document.querySelector("#inputNumber").value
+
+  attempts++
+
+  if (Number(chosenNumber) === randomNumber) toggleScreen()
+  else incorrectAttempts()
 }
 
 buttonTry.addEventListener("click", verifyIfAnswerMatch)
