1dc622153995022f1566eab9e1dd4f86af0ce48a
Claudiney <csld72k@gmail.com>
2024-01-31 22:11:34 -0300

    Implementa as funcionalidades de trocar de tela ao acertar o número e ao resetar o jogo e também a flexibilidade quanto a pluralidade da palavra try/tries no <h1> da tela de quando o usuário acerta o número

--- rocketseat/programa-explorer/stage-05/01-jogo-da-adivinhação/script.js
+++ rocketseat/programa-explorer/stage-05/01-jogo-da-adivinhação/script.js
@@ -2,7 +2,9 @@ let gameScreen = document.querySelector(".gameScreen")
 let winScreen = document.querySelector(".winScreen")
 // let inputNumber = document.querySelector("#inputNumber")
 let buttonTry = document.querySelector("#buttonTry")
-let randomNumber, attempts
+let buttonPlayAgain = document.querySelector("#buttonPlayAgain")
+let randomNumber
+let attempts = 0
 
 function generateRandomNumber() {
   return randomNumber = Math.round(Math.random() * 10)
@@ -12,19 +14,45 @@ randomNumber = generateRandomNumber()
 
 console.log(randomNumber)
 
+function toggleScreen() {
+  gameScreen.classList.toggle("disabled")
+  winScreen.classList.toggle("disabled")
+}
+
 function incorrectAttempts() {
   let formContainer = document.querySelector(".container")
   formContainer.style.animation = "incorrectAttemptsAnimation .2s 0s 6 alternate linear none"
   setTimeout(() => { formContainer.style.animation = "" }, 1200)
 }
 
-function verifyIfAnswerMatch() {
+function verifyIfAnswerMatch(Event) {
+  Event.preventDefault()
   let chosenNumber = document.querySelector("#inputNumber").value
+
+  if (chosenNumber.length === 0) return incorrectAttempts()
 
   attempts++
 
-  if (Number(chosenNumber) === randomNumber) toggleScreen()
+  if (Number(chosenNumber) === randomNumber) {
+    let winText = document.querySelector(".winScreen h1")
+    console.log(attempts)
+
+    attempts === 1 ? winText.innerHTML = `You got it right in ${attempts} try!` : winText.innerHTML = `You got it right in ${attempts} tries!`
+    toggleScreen()
+  }
   else incorrectAttempts()
 }
 
+function newGame() {
+  attempts = 0
+  document.querySelector("#inputNumber").value = ""
+  randomNumber = generateRandomNumber()
+  toggleScreen()
+}
+
+function eventToPrevent(event) {
+  event.preventDefault()
+}
+
 buttonTry.addEventListener("click", verifyIfAnswerMatch)
+buttonPlayAgain.addEventListener("click", newGame)
