d17833faab99474f1d3fa1fba48013dcb70ae33c
Claudiney <csld72k@gmail.com>
2024-02-10 18:58:42 -0300

    Implementa a funcionalidade de verificar se os campos de peso e altura estão preenchidos

--- rocketseat/programa-explorer/stage-05/02-projeto-imc/script.js
+++ rocketseat/programa-explorer/stage-05/02-projeto-imc/script.js
@@ -1,1 +1,10 @@
-console.log("Teste")
+let inputWeight = document.querySelector("#inputWeight")
+let inputHeight = document.querySelector("#inputHeight")
+let buttonCalculate = document.querySelector("#calculate")
+let buttonCloseModal = document.querySelector("#closeModal")
+
+buttonCalculate.addEventListener("click", calculateBMI)
+
+function checkIfTheInputsAreFilledIn() {
+  return (inputWeight.value.length > 0 && inputHeight.value.length > 0)
+}
