61ecfa883f64d4ba872e82e36d598d050ce81426
Claudiney <csld72k@gmail.com>
2024-02-10 19:13:09 -0300

    Implementa a funcionalidade de calcular o IMC

--- rocketseat/programa-explorer/stage-05/02-projeto-imc/script.js
+++ rocketseat/programa-explorer/stage-05/02-projeto-imc/script.js
@@ -1,5 +1,6 @@
 let inputWeight = document.querySelector("#inputWeight")
 let inputHeight = document.querySelector("#inputHeight")
+let BMIResultMessage = document.querySelector(".modal h1")
 let buttonCalculate = document.querySelector("#calculate")
 let buttonCloseModal = document.querySelector("#closeModal")
 
@@ -7,4 +8,15 @@ buttonCalculate.addEventListener("click", calculateBMI)
 
 function checkIfTheInputsAreFilledIn() {
   return (inputWeight.value.length > 0 && inputHeight.value.length > 0)
+}
+
+function calculateBMI(event) {
+  event.preventDefault()
+
+  if (checkIfTheInputsAreFilledIn()) {
+    let BMI = (inputWeight.value / ((inputHeight.value / 100) ** 2)).toFixed(1)
+    BMIResultMessage.innerText = `Your BMI is ${BMI}`
+    showModalOnOff()
+  }
+  else showError()
 }
