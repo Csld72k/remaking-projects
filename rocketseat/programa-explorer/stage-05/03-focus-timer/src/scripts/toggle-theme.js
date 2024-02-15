ba536a72458b296e9f521f8bfc3ff90bdca7853b
Claudiney <csld72k@gmail.com>
2024-02-15 19:36:15 -0300

    Ajustando as variáveis CSS

---
+++ rocketseat/programa-explorer/stage-05/03-focus-timer/src/scripts/toggle-theme.js
@@ -0,0 +1,10 @@
+let lightModeClass = "ph-bold ph-moon"
+let darkModeClass = "ph-bold ph-sun"
+let html = document.querySelector("html")
+let buttonToggleTheme = document.querySelector(".toggle-light-dark-mode")
+let icon = buttonToggleTheme.querySelector("i")
+
+buttonToggleTheme.addEventListener("click", () => {
+  html.classList.toggle("light")
+  html.classList.contains("light") ? icon.classList = lightModeClass : icon.classList = darkModeClass
+})
