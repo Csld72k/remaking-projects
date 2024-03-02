91a4b7a7489967234271a31c19868c820cb94e5e
Claudiney <csld72k@gmail.com>
2024-03-02 20:42:39 -0300

    Feat. Implementa a func. de deletar o usuário Implementa a funcionalidade de deletar o usuário da lista de favoritos e faz com que o usuário seja removido tanto dos dados da aplicação quanto da apresnetação da aplicação no navegador.

--- rocketseat/programa-explorer/stage-06/02-git-fav-v1/js/favorites.js
+++ rocketseat/programa-explorer/stage-06/02-git-fav-v1/js/favorites.js
@@ -2,11 +2,12 @@ export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
+    // this.delete()
   }
 
   load() {
 
-    this.entries = [
+    this.users = [
       {
         login: "csld72k",
         name: "Claudiney Silva",
@@ -20,6 +21,13 @@ export class Favorites {
         followers: "1234321"
       }
     ]
+
+  }
+
+  delete(userToDelete) {
+    const updatedUsers = this.users.filter(user => userToDelete.login !== user.login)
+
+    this.users = updatedUsers
 
   }
 }
@@ -36,7 +44,7 @@ export class FavoritesView extends Favorites {
   update() {
 
     this.removeAllUsers()
-    this.entries.forEach(user => {
+    this.users.forEach(user => {
       const row = this.createRow()
       row.querySelector(".user img").src = `https://github.com/${user.login}.png`
       row.querySelector(".user img").alt = `${user.name} image`
@@ -45,9 +53,18 @@ export class FavoritesView extends Favorites {
       row.querySelector(".repositories").textContent = user.public_repos
       row.querySelector(".followers").textContent = user.followers
 
+      row.querySelector(".remove").onclick = () => {
+
+        const confirmUserDeletion = confirm("Are you sure you want to remove this user?")
+
+        if (confirmUserDeletion) this.delete(user)
+        this.update()
+      }
+
       this.tbody.append(row)
 
     })
+
 
 
   }
@@ -56,7 +73,7 @@ export class FavoritesView extends Favorites {
 
     const tr = document.createElement("tr")
     tr.innerHTML = `
-      <td class="user">
+        <td class="user">
           <img src="" alt="">
           <a href="" target="_blank">
             <p></p>
