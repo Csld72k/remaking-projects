d4d24eb95d13f92ff880c52dfa72bcc2da22cebb
Claudiney <csld72k@gmail.com>
2024-03-17 16:01:01 -0300

    :construction: feat: inserir o usuário na tela

--- rocketseat/programa-explorer/stage-06/03-git-fav-v2/src/js/Favorites.js
+++ rocketseat/programa-explorer/stage-06/03-git-fav-v2/src/js/Favorites.js
@@ -3,6 +3,28 @@ export class Favorites {
   constructor(root) {
 
     this.root = document.querySelector(root)
+    this.load()
 
   }
+
+  load() {
+
+    this.users = [
+      {
+        login: "csld72k",
+        name: "Claudiney Silva",
+        public_repos: 1,
+        followers: 2
+      },
+      {
+        login: "diego3g",
+        name: "Diego Fernandes",
+        public_repos: 3,
+        followers: 4
+
+      }
+    ]
+
+  }
+
 }
