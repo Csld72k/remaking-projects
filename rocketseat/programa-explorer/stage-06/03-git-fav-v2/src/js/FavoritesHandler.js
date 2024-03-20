f51ed948ae8ed499cc7caadc0f0a48a96c13047f
Claudiney <csld72k@gmail.com>
2024-03-20 15:48:55 -0300

    :construction: feat: atualizar a tela e mostrar os usuários cadastrados

--- rocketseat/programa-explorer/stage-06/03-git-fav-v2/src/js/FavoritesHandler.js
+++ rocketseat/programa-explorer/stage-06/03-git-fav-v2/src/js/FavoritesHandler.js
@@ -6,13 +6,35 @@ export class FavoritesHandler extends FavoritesData {
 
     super(root)
     this.filledTbody = this.root.querySelector("tbody.filled-list")
+    this.update()
 
   }
 
+  update() {
+
+    this.loadUsers()
+    console.log(this.users)
+    this.users.forEach(user => {
+
+      const row = this.createRow()
+      row.querySelector("img").src = `https://github.com/${user.login}.png`
+      row.querySelector("a").src = `https://github.com/${user.login}`
+      row.querySelector(".name").innerText = user.name
+      row.querySelector(".username").innerText = user.login
+      row.querySelector(".repositories").innerText = user.public_repos
+      row.querySelector(".followers").innerText = user.followers
+
+      this.filledTbody.append(row)
+
+
+    })
+
+  }
 
   createRow() {
 
-    const row = `
+    const row = document.createElement("tr")
+    row.innerHTML = `
     <td class="user">
       <img class="user-image" src="" alt="Profile picture">
       <a href="" target="_blank">
@@ -30,7 +52,7 @@ export class FavoritesHandler extends FavoritesData {
     </td>
     `
 
-    this.filledTbody.createElement(row)
+    return row
 
   }
 
