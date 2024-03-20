00b201cd5157c39bc6597cabb8bb4b35f30c5fac
Claudiney <csld72k@gmail.com>
2024-03-20 15:17:12 -0300

    :construction: feat: criar a linha com dados do usuário

--- rocketseat/programa-explorer/stage-06/03-git-fav-v2/src/js/FavoritesHandler.js
+++ rocketseat/programa-explorer/stage-06/03-git-fav-v2/src/js/FavoritesHandler.js
@@ -5,7 +5,33 @@ export class FavoritesHandler extends FavoritesData {
   constructor(root) {
 
     super(root)
-
+    this.filledTbody = this.root.querySelector("tbody.filled-list")
 
   }
+
+
+  createRow() {
+
+    const row = `
+    <td class="user">
+      <img class="user-image" src="" alt="Profile picture">
+      <a href="" target="_blank">
+        <span class="name"></span>
+        <span class="username"></span>
+      </a>
+    </td>
+
+    <td class="repositories"></td>
+
+    <td class="followers"></td>
+
+    <td class="action">
+      <button class="remove">Remove</button>
+    </td>
+    `
+
+    this.filledTbody.createElement(row)
+
+  }
+
 }
