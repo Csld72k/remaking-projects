ac4601c493e01169e9b74eb724bff04e2bcd0f95
Claudiney <csld72k@gmail.com>
2024-10-28 16:47:38 -0300

    :construction: Feat: Learning about query params.

--- rocketseat/programa-explorer/stage-08/api/src/server.js
+++ rocketseat/programa-explorer/stage-08/api/src/server.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/message/:id/:user', (request, response) => {
-  const { id, user } = request.params
+// route params
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
 
   response.send(`
     Hello, world! 
@@ -11,6 +12,14 @@ app.get('/message/:id/:user', (request, response) => {
     User: ${user}`)
 })
 
+// query params
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Page: ${page}. Limit: ${limit}`)
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
