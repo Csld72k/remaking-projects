a48809d9f78d20d26117dde78c8ece4f1cdea88a
Claudiney <csld72k@gmail.com>
2024-10-31 20:32:47 -0300

    :construction: Feat: Inserting middleware.

--- rocketseat/programa-explorer/stage-08/api/src/routes/users.routes.js
+++ rocketseat/programa-explorer/stage-08/api/src/routes/users.routes.js
@@ -1,11 +1,30 @@
 const { Router } = require("express");
 
-const UsersController = require("../controllers/UsersController")
+const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
+
+
+
+
+
+function myMiddleware(request, response, next) {
+  console.log("You passed through the middleware!");
+
+  if (!request.body.isAdmin) return response.json({ message: "User unauthorized." })
+
+  next()
+
+}
+
+
+
+
+
+
 const usersController = new UsersController();
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/", myMiddleware, usersController.create);
 
 module.exports = usersRoutes;
--- rocketseat/programa-explorer/stage-08/api/src/server.js
+++ rocketseat/programa-explorer/stage-08/api/src/server.js
@@ -11,4 +11,3 @@ app.use(routes)
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
