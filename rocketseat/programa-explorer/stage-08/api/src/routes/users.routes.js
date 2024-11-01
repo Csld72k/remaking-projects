3007e575546b40d548b5566000b71db473bd7086
Claudiney <csld72k@gmail.com>
2024-10-31 21:06:20 -0300

    :construction: Feat: Inserting a class to treat errors

--- rocketseat/programa-explorer/stage-08/api/src/routes/users.routes.js
+++ rocketseat/programa-explorer/stage-08/api/src/routes/users.routes.js
@@ -4,27 +4,8 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-
-
-
-
-
-function myMiddleware(request, response, next) {
-  console.log("You passed through the middleware!");
-
-  if (!request.body.isAdmin) return response.json({ message: "User unauthorized." })
-
-  next()
-
-}
-
-
-
-
-
-
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
